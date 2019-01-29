a2320637747c7abb21e36676014b8e54821d8f1c
Yamin05114 <liyamin0514@gmail.com>
2019-01-28 19:53:00 -0500

    Update compressor.py

--- compressor.py
+++ compressor.py
@@ -9,12 +9,15 @@ class Compressor(object):
         self.dropout_rates = {}
 
         self.count_layers()
-
+        
+        # 初始所有层，每一层是一个None
         self.weight_masks = [None for _ in range(self.num_layers)]
         self.bias_masks = [None for _ in range(self.num_layers)]
-
+        
         self.cuda = cuda
-
+    
+    # 计算层数，卷积和fc就算layer
+    # drop out不算layer但是记录概率
     def count_layers(self):
         for m in self.model.modules():
             if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
@@ -34,13 +37,17 @@ class Compressor(object):
 
         for m in self.model.modules():
             if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                
+                # weights 总数
                 num = torch.numel(m.weight.data)
-
+                
+                # 卷积层 alpha们[0.015 0.2 0.2 0.2]
                 if type(m) == nn.Conv2d:
                     if index == 0:
                         alpha = 0.015
                     else:
                         alpha = 0.2
+                # fc layers [1,1,1,...,0.25]
                 else:
                     if index == self.num_layers - 1:
                         alpha = 0.25
@@ -48,10 +55,11 @@ class Compressor(object):
                         alpha = 1
 
                 # use a byteTensor to represent the mask and convert it to a floatTensor for multiplication
+                # mask删除的门限是weight自己的标准差
                 weight_mask = torch.ge(m.weight.data.abs(), alpha * m.weight.data.std()).type('torch.FloatTensor')
                 if self.cuda:
                     weight_mask = weight_mask.cuda()
-                self.weight_masks[index] = weight_mask
+                self.weight_masks[index] = weight_mask  # 存上 weight mask
 
                 bias_mask = torch.ones(m.bias.data.size())
                 if self.cuda:
@@ -60,13 +68,14 @@ class Compressor(object):
                 # for all kernels in the conv2d layer, if any kernel is all 0, set the bias to 0
                 # in the case of linear layers, we search instead for zero rows
                 for i in range(bias_mask.size(0)):
-                    if len(torch.nonzero(weight_mask[i]).size()) == 0:
+                    if len(torch.nonzero(weight_mask[i]).size()) == 0:  # 非零的位置，如果没有biasmask直接清零。
                         bias_mask[i] = 0
-                self.bias_masks[index] = bias_mask
+                self.bias_masks[index] = bias_mask  # 存上bias mask
 
                 index += 1
 
-                layer_pruned = num - torch.nonzero(weight_mask).size(0)
+                layer_pruned = num - torch.nonzero(weight_mask
+                                                  ).size(0)
                 logging.info('number pruned in weight of layer %d: %.3f %%' % (index, 100 * (layer_pruned / num)))
                 bias_num = torch.numel(bias_mask)
                 bias_pruned = bias_num - torch.nonzero(bias_mask).size(0)
